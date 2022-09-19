c652351d9cf111632f71c9faaaf2bc81ccc413ad
George Stavrinos <gstavrinos@protonmail.com>
2022-09-19 23:53:38 +0300

    Fixed stupid typo

--- ros2_installer.bash
+++ ros2_installer.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pip install rocker
-sudo docker build --no-cache --pull -t ros2_ez -f includes/ ros2_Dockerfile .
+sudo docker build --no-cache --pull -t ros2_ez -f includes/ros2_Dockerfile .
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )'/ros2-ez'
 string_for_bashrc='export PATH="$PATH:'$SCRIPT_DIR'"'
 if ! $( grep -Fx "$string_for_bashrc" ~/.bashrc )
