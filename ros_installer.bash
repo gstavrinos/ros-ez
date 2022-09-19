0555212b8b55bd56e6c04aa1da630fe8e97e7e3c
George Stavrinos <gstavrinos@protonmail.com>
2022-09-19 09:32:38 +0300

    Made scripts executable and uncommented required lines
    Testing was easier with these lines commented out.

--- ros_installer.bash
+++ ros_installer.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-# sudo pip install rocker
-# sudo docker build --no-cache --pull -t ros_ez -f includes/ros_Dockerfile .
+sudo pip install rocker
+sudo docker build --no-cache --pull -t ros_ez -f includes/ros_Dockerfile .
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )'/ros-ez'
 string_for_bashrc='export PATH="$PATH:'$SCRIPT_DIR'"'
 if ! $( grep -Fx "$string_for_bashrc" ~/.bashrc )
