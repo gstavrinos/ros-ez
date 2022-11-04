9d885a0b41a4dce53a636a1c20ca22d4c1b61371
George Stavrinos <gstavrinos@protonmail.com>
2022-11-04 09:12:47 +0200

    Added ability for multiple workspaces in ROS2

--- includes/ros2_entrypoint.bash
+++ includes/ros2_entrypoint.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
 . /opt/ros/humble/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-cd /opt/ros/ros2_ws
-rosdep install -i --from-path src --rosdistro humble -y
-colcon build --symlink-install
-. /opt/ros/ros2_ws/install/setup.bash
+while read -r line
+do
+    bl="$(basename $line)"
+    cd /opt/ros/$bl
+    rosdep install -i --from-path src --rosdistro humble -y
+    colcon build --symlink-install
+    . /opt/ros/$bl/install/setup.bash
+done < /opt/ros/ros2_ws.txt
 exec "$@"
