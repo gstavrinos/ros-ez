fd8c624589a24b3856a7c106152a55d6e6876190
George Stavrinos <gstavrinos@protonmail.com>
2022-12-27 18:08:51 +0200

    Check if new packages are needed and update apt before proceeding to install

--- includes/ros2_entrypoint.bash
+++ includes/ros2_entrypoint.bash
@@ -5,6 +5,9 @@ while read -r line
 do
     bl="$(basename $line)"
     cd /opt/ros/$bl
+    if rosdep check -i --from-path src --rosdistro humble -y | grep -q 'System dependencies have not been satisfied'; then
+        apt update
+    fi
     rosdep install -i --from-path src --rosdistro humble -y
     colcon build --symlink-install
     . /opt/ros/$bl/install/setup.bash
