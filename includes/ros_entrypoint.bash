dbfae8831158e98369261865f079350fcfea83d2
George Stavrinos <gstavrinos@protonmail.com>
2022-12-28 18:55:21 +0200

    Check if new packages are needed and update apt before proceeding to install (ROS1 version)

--- includes/ros_entrypoint.bash
+++ includes/ros_entrypoint.bash
@@ -4,6 +4,9 @@ while read -r line
 do
     bl="$(basename $line)"
     cd /opt/ros/$bl
+    if rosdep check -i --from-path src --rosdistro noetic -y | grep -q 'System dependencies have not been satisfied'; then
+        apt update
+    fi
     rosdep install -i --from-path src --rosdistro noetic -y
     catkin_make
     . /opt/ros/$bl/devel/setup.bash
