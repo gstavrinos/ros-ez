d50c87f12f063684fc277d27024fea66c1275e4f
George Stavrinos <gstavrinos@protonmail.com>
2024-10-30 18:14:52 +0200

    Now using docker run once and then using exec for multiple shells and commands
    The diff is flooded mostly by my auto formatter. This should settle down in future commits

--- internal/helpers.bash
+++ internal/helpers.bash
@@ -2,16 +2,18 @@
 # Export everything in this file
 set -a
 
-known_params=("force-integrated" "clear-locks" "skip-compilation" "non-interactive" "no-sound")
-known_params_short=("fi" "cl" "sc" "ni" "ns")
+known_params=("force-integrated" "clear-locks" "skip-compilation" "non-interactive" "no-sound" "kill-container")
+known_params_short=("fi" "cl" "sc" "ni" "ns" "kc")
 signal_list=(INT QUIT TSTP TERM HUP KILL)
 colour_end='\033[0m'
 colour_red='\033[0;31m'
 colour_green='\033[0;32m'
 colour_orange='\033[0;33m'
 colour_blue='\033[0;34m'
-lock_prefix=".rosez-"
-lock_suffix=".lock"
+exec_lock_prefix=".rosez-exec-"
+exec_lock_suffix=".lock"
+read_lock_prefix=".rosez-"
+read_lock_suffix=".lock"
 
 rosezv[0]=""
 workspaces[0]=""
@@ -25,49 +27,50 @@ title="rosez"
 dialog_command="dialog --title \"$title\" --clear"
 # Function that simpifies the process of creating dialog "screens"
 function run_dialog_command {
-        tmpfile=`tempfile 2>/dev/null` || tmpfile=/tmp/test$$
-        trap "rm -f $tmpfile" $signal_list
-        if [ -n "$3" ] && [ $3 -eq 0 ]; then
-            eval "$dialog_command $1 \"$2\" 20 61 \"$4\" 2>$tmpfile"
+  tmpfile=$(tempfile 2>/dev/null) || tmpfile=/tmp/test$$
+  trap "rm -f $tmpfile" $signal_list
+  if [ -n "$3" ] && [ $3 -eq 0 ]; then
+    eval "$dialog_command $1 \"$2\" 20 61 \"$4\" 2>$tmpfile"
 
-        else
-            eval "$dialog_command $1 \"$2\" 20 60 $3 $4 2>$tmpfile"
-        fi
-        retval=$?
-        if [ $retval -eq 0 ]; then
-            choice=$(cat $tmpfile)
-        else
-            clear
-            exit 0
-        fi
-    }
+  else
+    eval "$dialog_command $1 \"$2\" 20 60 $3 $4 2>$tmpfile"
+  fi
+  retval=$?
+  if [ $retval -eq 0 ]; then
+    choice=$(cat $tmpfile)
+  else
+    clear
+    exit 0
+  fi
+}
 # This function read a txt file with all the supported ros versions along with their configuration
 function get_supported_versions {
-    helper_script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-    supported_versions_file=$helper_script_dir/supported_versions.txt
-    vnum=0
-    while read -r line; do
-        while IFS=',' read -ra line_arr; do
-            if [ $vnum -gt 0 ]; then
-                index=$(( $vnum-1 ))
-                rosezv[$index]="${line_arr[0]}"
-                workspaces[$index]="${line_arr[1]}"
-                volumes[$index]="${line_arr[2]}"
-                distros[$index]="${line_arr[3]}"
-                image_names[$index]="${line_arr[4]}"
-                executable_folder_names[$index]="${line_arr[5]}"
-                dockerfiles[$index]="${line_arr[6]}"
-            fi
-            vnum=$(( $vnum+1 ))
-        done <<< $line
-    done < $supported_versions_file
+  helper_script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+  supported_versions_file=$helper_script_dir/supported_versions.txt
+  vnum=0
+  while read -r line; do
+    while IFS=',' read -ra line_arr; do
+      if [ $vnum -gt 0 ]; then
+        index=$(($vnum - 1))
+        rosezv[$index]="${line_arr[0]}"
+        workspaces[$index]="${line_arr[1]}"
+        volumes[$index]="${line_arr[2]}"
+        distros[$index]="${line_arr[3]}"
+        image_names[$index]="${line_arr[4]}"
+        executable_folder_names[$index]="${line_arr[5]}"
+        dockerfiles[$index]="${line_arr[6]}"
+      fi
+      vnum=$(($vnum + 1))
+    done <<<$line
+  done <$supported_versions_file
 }
 
 # This function is used to trap signals
 function signal_handler() {
-    echo -e "${colour_red}Execution was aborted, deleting my lock_file ($lock_file)$colour_end"
-    rm -f $lockation/$lock_file
-    exit
+  echo -e "${colour_red}Execution was aborted, deleting my lock files ($read_lock_file and $exec_lock_file)$colour_end"
+  rm -f $lockation/$read_lock_file
+  rm -f $lockation/$exec_lock_file
+  exit
 }
 
 # This function is used to check intermediate commands'
@@ -75,8 +78,8 @@ function signal_handler() {
 # This is a required behaviour, in order
 # to enter the shell (not exit) when a build fails.
 function intermediate_error_handler() {
-    if [ $1 -gt 0 ]; then
-        echo -e "${colour_red}Intermediate process aborted!$colour_end"
-        signal_handler
-    fi
+  if [ $1 -gt 0 ]; then
+    echo -e "${colour_red}Intermediate process aborted!$colour_end"
+    signal_handler
+  fi
 }
--- internal/rosez_exec.bash
+++ internal/rosez_exec.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 cwd=$(pwd)
 cd $cwd
 source $SCRIPT_DIR/helpers.bash
@@ -8,8 +8,11 @@ uptime_date="$(uptime -s | sed "s/[: ]/_/g")"
 # Using uptime_date I can see if a lock file is older
 # than the current uptime and thus safely delete it.
 # This should enable the "a reboot should fix it" behaviour
-earliest_possible_lock_file="$lock_prefix$uptime_date$lock_suffix"
-lock_file="$lock_prefix$now$lock_suffix"
+earliest_possible_read_lock_file="$read_lock_prefix$uptime_date$read_lock_suffix"
+earliest_possible_exec_lock_file="$exec_lock_prefix$uptime_date$exec_lock_suffix"
+read_lock_file="$read_lock_prefix$now$read_lock_suffix"
+exec_lock_file="$exec_lock_prefix$now$exec_lock_suffix"
+# TODO: investigate all parameters and how they are affected with exec VS run
 rosws_file="ros2_ws.txt"
 rosez_vol="ros2ez-volume"
 ros="humble"
@@ -20,143 +23,168 @@ clear_locks=0
 skip_compilation=0
 non_interactive=0
 no_sound=0
+kill_container=0
 lockdir=""
 userid=$(id -u)
 if [ "$1" == "1" ]; then
-    rosws_file="ros_ws.txt"
-    rosez_vol="rosez-volume"
-    ros="noetic"
-    ros_image="ros_ez"
+  rosws_file="ros_ws.txt"
+  rosez_vol="rosez-volume"
+  ros="noetic"
+  ros_image="ros_ez"
 elif [ "$1" == "3" ]; then
-    rosws_file="rosm_ws.txt"
-    rosez_vol="rosezm-volume"
-    ros="melodic"
-    ros_image="ros_ezm"
+  rosws_file="rosm_ws.txt"
+  rosez_vol="rosezm-volume"
+  ros="melodic"
+  ros_image="ros_ezm"
 elif [ "$1" == "4" ]; then
-    rosws_file="ros2f_ws.txt"
-    rosez_vol="ros2ezf-volume"
-    ros="foxy"
-    ros_image="ros2_ezf"
+  rosws_file="ros2f_ws.txt"
+  rosez_vol="ros2ezf-volume"
+  ros="foxy"
+  ros_image="ros2_ezf"
 fi
 shift
 trap 'signal_handler' $signal_list
 volumes=""
 lockation=""
-while read -r line
-do
-    wsdir=$(eval echo -e "$line")
-    if [ ! -d "$wsdir" ]; then
-        echo "$wsdir not found, creating it"
-        mkdir -p $wsdir/src
-    fi
-    volumes=$volumes"--volume $wsdir:/opt/ros/$(basename $wsdir) "
-    if [[ -z "$lockation" ]]; then
-        lockation=$wsdir
-    fi
-done < $SCRIPT_DIR/../includes/$rosws_file
+while read -r line; do
+  wsdir=$(eval echo -e "$line")
+  if [ ! -d "$wsdir" ]; then
+    echo "$wsdir not found, creating it"
+    mkdir -p $wsdir/src
+  fi
+  volumes=$volumes"--volume $wsdir:/opt/ros/$(basename $wsdir) "
+  if [[ -z "$lockation" ]]; then
+    lockation=$wsdir
+  fi
+done <$SCRIPT_DIR/../includes/$rosws_file
 # I am not using the index here,
 # but I make sure that I cycle through
 # the (at most) first i parameters
 # to catch all the flags
 for i in "${!known_params[@]}"; do
-    if [ "${known_params[0]}" == "$1" ] || [ "${known_params_short[0]}" == "$1" ]; then
-        gpu_param="--device /dev/dri/card0"
-        shift
-    elif [ "${known_params[1]}" == "$1" ] || [ "${known_params_short[1]}" == "$1" ]; then
-        clear_locks=1
-        shift
-    elif [ "${known_params[2]}" == "$1" ] || [ "${known_params_short[2]}" == "$1" ]; then
-        skip_compilation=1
-        shift
-    elif [ "${known_params[3]}" == "$1" ] || [ "${known_params_short[3]}" == "$1" ]; then
-        non_interactive=1
-        shift
-    elif [ "${known_params[4]}" == "$1" ] || [ "${known_params_short[4]}" == "$1" ]; then
-        no_sound=1
-        shift
-    else
-        break
-    fi
+  if [ "${known_params[0]}" == "$1" ] || [ "${known_params_short[0]}" == "$1" ]; then
+    gpu_param="--device /dev/dri/card0"
+    shift
+  elif [ "${known_params[1]}" == "$1" ] || [ "${known_params_short[1]}" == "$1" ]; then
+    clear_locks=1
+    shift
+  elif [ "${known_params[2]}" == "$1" ] || [ "${known_params_short[2]}" == "$1" ]; then
+    skip_compilation=1
+    shift
+  elif [ "${known_params[3]}" == "$1" ] || [ "${known_params_short[3]}" == "$1" ]; then
+    non_interactive=1
+    shift
+  elif [ "${known_params[4]}" == "$1" ] || [ "${known_params_short[4]}" == "$1" ]; then
+    no_sound=1
+    shift
+  elif [ "${known_params[5]}" == "$1" ] || [ "${known_params_short[5]}" == "$1" ]; then
+    kill_container=1
+    shift
+  else
+    break
+  fi
 done
 if [ -z "$gpu_param" ]; then
-    if grep -q "nvidia" <<< "$gpu_string" || grep -q "Nvidia" <<< "$gpu_string" || grep -q "NVIDIA" <<< "$gpu_string"
-    then
-        gpu_param="--gpus all"
-    elif grep -q "intel" <<< "$gpu_string" || grep -q "Intel" <<< "$gpu_string" || grep -q "INTEL" <<< "$gpu_string"
-    then
-        gpu_param="--device /dev/dri/card0"
-    else
-        echo "No Nvidia or Intel GPU found. This case has not been investigated yet. GUI integration might be broken. (Good luck!)"
-    fi
+  if grep -q "nvidia" <<<"$gpu_string" || grep -q "Nvidia" <<<"$gpu_string" || grep -q "NVIDIA" <<<"$gpu_string"; then
+    gpu_param="--gpus all"
+  elif grep -q "intel" <<<"$gpu_string" || grep -q "Intel" <<<"$gpu_string" || grep -q "INTEL" <<<"$gpu_string"; then
+    gpu_param="--device /dev/dri/card0"
+  else
+    echo "No Nvidia or Intel GPU found. This case has not been investigated yet. GUI integration might be broken. (Good luck!)"
+  fi
 fi
 if [ $clear_locks -gt 0 ]; then
-    echo -e "${colour_orange}I was passed the clear-locks flag. Deleting all lock files...$colour_end"
-    rm -f $lockation/$lock_prefix*$lock_suffix
+  echo -e "${colour_orange}I was passed the clear-locks flag. Deleting all lock files...$colour_end"
+  rm -f $lockation/$read_lock_prefix*$read_lock_suffix
+fi
+if [ $kill_container -gt 0 ]; then
+  echo -e "${colour_orange}I was passed the kill-container flag. Killing (and deleting) my container ($ros_image)...$colour_end"
+  docker rm -f $ros_image >/dev/null 2>&1
 fi
 echo $gpu_param
-touch $lockation/$lock_file
+touch $lockation/$read_lock_file
 locked=1
 while [ $locked -gt 0 ]; do
-    found_lock="$(find $lockation -maxdepth 1 -name "$lock_prefix*$lock_suffix" -print | sort | head -1)"
-    # The found_lock should be empty only
-    # in the (rare?) case of enabling the 
-    # clear-locks (cl) flag from another rosez process
-    # ---
-    # A new lock_file is created, since the other rosez
-    # process will instantly get priority with the cl flag.
-    echo $found_lock
-    if [[ -z "$found_lock" ]]; then
-        now="$(date +'%Y-%m-%d_%H_%M_%S_%N')"
-        lock_file="$lock_prefix$now$lock_suffix"
-        touch $lockation/$lock_file
-        found_lock=$lockation/$lock_file
-    fi
-    fl="$(basename $found_lock)"
-    echo -e "${colour_blue}Processing: $fl$colour_end"
-    if [ "$fl" \< "$earliest_possible_lock_file" ]; then
-        echo -e "${colour_orange}Found problematic lock file ($fl)! Deleting it... (Lock file older than uptime)$colour_end"
-        sudo rm $found_lock
-    elif [ "$fl" \< "$lock_file" ]; then
-        echo -e "${colour_orange}Found earlier lock file ($fl) than my own ($lock_file)!\nWaiting...$colour_end"
-        sleep 3
-    elif [ "$fl" == "$lock_file" ]; then
-        echo -e "${colour_green}My lock file ($lock_file) is the earliest found. Continuing...$colour_end"
-        locked=0
-    fi
+  found_lock="$(find $lockation -maxdepth 1 -name "$read_lock_prefix*$read_lock_suffix" -print | sort | head -1)"
+  # The found_lock should be empty only
+  # in the (rare?) case of enabling the
+  # clear-locks (cl) flag from another rosez process
+  # ---
+  # A new lock_file is created, since the other rosez
+  # process will instantly get priority with the cl flag.
+  echo $found_lock
+  if [[ -z "$found_lock" ]]; then
+    now="$(date +'%Y-%m-%d_%H_%M_%S_%N')"
+    read_lock_file="$read_lock_prefix$now$read_lock_suffix"
+    touch $lockation/$read_lock_file
+    found_lock=$lockation/$read_lock_file
+  fi
+  fl="$(basename $found_lock)"
+  echo -e "${colour_blue}Processing: $fl$colour_end"
+  if [[ "$fl" < "$earliest_possible_read_lock_file" ]]; then
+    echo -e "${colour_orange}Found problematic lock file ($fl)! Deleting it... (Lock file older than uptime)$colour_end"
+    rm $found_lock
+  elif [ "$fl" \< "$read_lock_file" ]; then
+    echo -e "${colour_orange}Found earlier lock file ($fl) than my own ($read_lock_file)!\nWaiting...$colour_end"
+    sleep 3
+  elif [ "$fl" == "$read_lock_file" ]; then
+    echo -e "${colour_green}My lock file ($read_lock_file) is the earliest found. Continuing...$colour_end"
+    locked=0
+  fi
 done
 bloom_file=/home/$USER/.config/bloom
 gitconfig_file=/home/$USER/.gitconfig
 ssh_folder=/home/$USER/.ssh
-if [ ! -f  $bloom_file ]; then
-    touch $bloom_file
+if [ ! -f $bloom_file ]; then
+  touch $bloom_file
 fi
-if [ ! -f  $gitconfig_file ]; then
-    touch $gitconfig_file
+if [ ! -f $gitconfig_file ]; then
+  touch $gitconfig_file
 fi
-if [ ! -d  $ssh_folder ]; then
-    mkdir $ssh_folder
+if [ ! -d $ssh_folder ]; then
+  mkdir $ssh_folder
 fi
 intermediate_error_handler $?
 sound="-v /run/user/$userid/pulse:/run/user/$userid/pulse --device /dev/snd -e PULSE_SERVER=unix:/run/user/$userid/pulse/native -v /run/user/$userid/pulse/native:/run/user/$userid/pulse/native"
 if [ $no_sound -gt 0 ]; then
-    sound=""
+  sound=""
 fi
 it="-it"
 if [ $non_interactive -gt 0 ]; then
-    it=""
+  it=""
 fi
 xauthf="/tmp/.$ros_image-$now.xauth"
 touch $xauthf
 intermediate_error_handler $?
 /bin/bash -c "xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $xauthf nmerge -"
 intermediate_error_handler $?
-x="docker run --ulimit nofile=1024:524288 --rm $it -u $userid --ipc=host --privileged --network host $gpu_param $sound --group-add dialout --group-add video --group-add audio -v $rosez_vol-bin:/bin -v $rosez_vol-etc:/etc/ -v $rosez_vol-home:/home/ -v $rosez_vol-lib:/lib/ -v $rosez_vol-lib64:/lib64/ -v /mnt:/mnt -v $rosez_vol-opt:/opt/ -v $rosez_vol-root:/root/ -v /run:/run -v $rosez_vol-sbin:/sbin/ -v $rosez_vol-srv:/srv/ -v $rosez_vol-usr:/usr -v $rosez_vol-var:/var -v $rosez_vol:/opt/ros/$ros -v $SCRIPT_DIR/../includes/$rosws_file:/opt/ros/$rosws_file $volumes -v $SCRIPT_DIR/entrypoint.bash:/home/rosez_user/.bashrc -v /sys:/sys -v /dev:/dev -v $bloom_file:/home/rosez_user/.config/bloom -v $gitconfig_file:/home/rosez_user/.gitconfig -v $ssh_folder:/home/rosez_user/.ssh -v $SCRIPT_DIR/supported_versions.txt:/home/rosez_user/supported_versions.txt -v $SCRIPT_DIR/helpers.bash:/home/rosez_user/helpers.bash -v /:$HOME/.$rosez_vol -e DISPLAY -e TERM -e QT_X11_NO_MITSHM=1 -e XAUTHORITY=$xauthf -v $xauthf:$xauthf -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/localtime:/etc/localtime:ro $ros_image:latest"
+container_id=$(docker ps -q --filter "name=$ros_image")
+found_lock="$(find $lockation -maxdepth 1 -name "$exec_lock_prefix*$exec_lock_suffix" -print | sort | head -1)"
+fl="$(basename "$found_lock")"
+if [[ -z "$container_id" || "$fl" < "$earliest_possible_exec_lock_file" ]]; then
+  docker rm -f $ros_image >/dev/null 2>&1
+  intermediate_error_handler $?
+  x="docker run --name $ros_image --ulimit nofile=1024:524288 -d -u $userid --ipc=host --privileged --network host $gpu_param $sound --group-add dialout --group-add video --group-add audio -v $rosez_vol-bin:/bin -v $rosez_vol-etc:/etc/ -v $rosez_vol-home:/home/ -v $rosez_vol-lib:/lib/ -v $rosez_vol-lib64:/lib64/ -v /mnt:/mnt -v $rosez_vol-opt:/opt/ -v $rosez_vol-root:/root/ -v /run:/run -v $rosez_vol-sbin:/sbin/ -v $rosez_vol-srv:/srv/ -v $rosez_vol-usr:/usr -v $rosez_vol-var:/var -v $rosez_vol:/opt/ros/$ros -v $SCRIPT_DIR/../includes/$rosws_file:/opt/ros/$rosws_file $volumes -v $SCRIPT_DIR/entrypoint.bash:/home/rosez_user/.bashrc -v /sys:/sys -v /dev:/dev -v $bloom_file:/home/rosez_user/.config/bloom -v $gitconfig_file:/home/rosez_user/.gitconfig -v $ssh_folder:/home/rosez_user/.ssh -v $SCRIPT_DIR/supported_versions.txt:/home/rosez_user/supported_versions.txt -v $SCRIPT_DIR/helpers.bash:/home/rosez_user/helpers.bash -v /:$HOME/.$rosez_vol -e DISPLAY -e TERM -e QT_X11_NO_MITSHM=1 -e XAUTHORITY=$xauthf -v $xauthf:$xauthf -v /tmp/.X11-unix:/tmp/.X11-unix -v /etc/localtime:/etc/localtime:ro $ros_image:latest tail -f /dev/null"
+  intermediate_error_handler $?
+  echo -e "${colour_blue}$ros_image daemon not found.${colour_end}\nExecuting:\n---\n$x\n---"
+  eval "$x"
+  container_id=$(docker ps -q --filter "name=$ros_image")
+  intermediate_error_handler $?
+fi
+x="docker exec $it $container_id"
 intermediate_error_handler $?
-extras="env $ENV LOCKFILE=$lock_file SKIPCOMPILATION=$skip_compilation /bin/bash"
+extras="env $ENV LOCKFILE=$read_lock_file SKIPCOMPILATION=$skip_compilation /bin/bash"
 if [ $# -gt 0 ]; then
-    extras=$extras" -c \"source /home/rosez_user/.bashrc && $* \""
+  extras=$extras" -c \"source /home/rosez_user/.bashrc && $* \""
 fi
 x="$x $extras"
 intermediate_error_handler $?
-printf "Executing:\n---\n$x\n---\n"
+touch $lockation/$exec_lock_file
+echo -e "Executing:\n---\n$x\n---"
 eval "$x"
+rm -f $lockation/$exec_lock_file
+found_lock="$(find $lockation -maxdepth 1 -name "$exec_lock_prefix*$exec_lock_suffix" -print | sort | head -1)"
+if [[ -z "$found_lock" ]]; then
+  echo -e "\n${colour_blue}This shell was the last one using the $ros_image container. Removing it${colour_end}"
+  docker rm -f $ros_image >/dev/null 2>&1
+fi
